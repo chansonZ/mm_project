518d29024fb570748e4a5f5f564b8f0a170f7624
Samuel Lampa <samuel.lampa@gmail.com>
2015-06-28 04:14:36 +0200

    Simple hard-coded CreateFolds now works

--- experiments/2015-06-27-135550-cross-validation/crossval.py
+++ experiments/2015-06-27-135550-cross-validation/crossval.py
@@ -1,29 +1,39 @@
 import luigi
 import sciluigi as sl
+import time
+import random
+import subprocess as sub
 
 # ====================================================================================================
 
 class CrossValRawData(sl.ExternalTask):
+
+    file_name = luigi.Parameter()
+
     def out_dataset(self):
-        return sl.TargetInfo(self, 'data/cv_rawdata.txt')
+        return sl.TargetInfo(self, 'data/{f}'.format(f=self.file_name))
 
 # ====================================================================================================
 
-class CrossValSplitByLines(sl.Task):
-
-    # INPUT TARGETS
-    in_dataset = None
+class CreateFolds(sl.Task):
 
     # TASK PARAMETERS
     folds_count = luigi.Parameter()
+    fold_index = luigi.Parameter()
+    seed = luigi.Parameter()
 
-    def output(self):
-        return { 'split_%d' % i: luigi.LocalTarget(self.get_input('dataset_target').path + '.split_%d' % i) for i in xrange(int(self.splits_count)) }
+    # TARGETS
+    in_dataset = None
+    def out_traindata(self):
+        return sl.TargetInfo(self, self.in_dataset().path + '.fold{0:02}_train'.format(self.fold_index))
+
+    def out_testdata(self):
+        return sl.TargetInfo(self, self.in_dataset().path + '.fold{0:02}_test'.format(self.fold_index))
 
     # CONVENIENCE METHODS
     def count_lines(self, filename):
-        status, output = self.lx(['cat', filename, '|', 'wc -l'])
-        return int(output)
+        output = sub.check_output('wc -l %s' % filename, shell=True)
+        return int(output.split(' ')[0])
 
     def remove_dict_key(self, orig_dict, key):
         new_dict = dict(orig_dict)
@@ -34,9 +44,9 @@ class CrossValSplitByLines(sl.Task):
         return [line for i, line in enumerate(dataset) if i in line_nos]
 
     def run(self):
-        linecnt = self.count_lines(self.get_input('dataset_target').path)
+        linecnt = self.count_lines(self.in_dataset().path)
         line_nos = [i for i in xrange(1, linecnt)]
-        random.shuffle(line_nos)
+        random.shuffle(line_nos, lambda: self.seed)
 
         splits_as_linenos = {}
 
@@ -46,10 +56,23 @@ class CrossValSplitByLines(sl.Task):
         for i in xrange(int(self.folds_count)):
             splits_as_linenos[i] = line_nos[i * set_size : (i+1) * set_size]
 
-        for i, split_id in enumerate(self.output()):
-            with self.get_input('dataset_target').open() as infile, self.output()[split_id].open('w') as outfile:
-                lines = self.pick_lines(infile, splits_as_linenos[i])
-                outfile.writelines(lines)
+        # Write test file
+        test_linenos = splits_as_linenos[self.fold_index]
+        with self.in_dataset().open() as infile, self.out_testdata().open('w') as testfile:
+            for lineno, line in enumerate(infile):
+                if lineno in test_linenos:
+                    testfile.write(line)
+
+        # Write train file
+        train_splits_linenos = self.remove_dict_key(splits_as_linenos, self.fold_index)
+        train_linenos = []
+        for k, v in splits_as_linenos.iteritems():
+            train_linenos.extend(v)
+        with self.in_dataset().open() as infile, self.out_traindata().open('w') as trainfile:
+            for lineno, line in enumerate(infile):
+                if lineno in train_linenos:
+                    trainfile.write(line)
+
 
 # ====================================================================================================
 
@@ -61,6 +84,9 @@ class MergeTrainFolds(sl.Task):
 # ====================================================================================================
 
 class MockTrain(sl.Task):
+
+    in_traindata = None
+
     def run(self):
         time.sleep(1)
 
--- experiments/2015-06-27-135550-cross-validation/workflow.py
+++ experiments/2015-06-27-135550-cross-validation/workflow.py
@@ -12,7 +12,6 @@ class CrossValidate(sl.WorkflowTask):
     For now, a sketch on how to implement Cross-Validation as a sub-workflow components
     '''
 
-
     # PARAMETERS
     task = luigi.Parameter()
     folds_count = luigi.IntParameter()
@@ -21,39 +20,40 @@ class CrossValidate(sl.WorkflowTask):
         # Hard-code this for now ...
         rawdata = sl.new_task(CrossValRawData, 'rawdata', self, file_name='crossval_rawdata.txt')
 
-        # Create the initial component that splits the initial dataset into
-        # k equal splits, or folds ...
-        split = sl.new_task(CrossValSplitByLines, 'split', self, splits_count = self.folds_count)
-        split.in_dataset = rawdata.out_dataset
-
         # Branch the workflow into one branch per fold
         fold_tasks = {}
-        for i in xrange(self.folds_count):
-            # A task that will merge all folds except the one left out for testing,
-            # ... into a training data set, and just pass on the one left out, as
-            # the test data set.
-            merge = sl.new_task(MergeTrainFolds, 'merge', self)
-            merge = split
+        #for i in xrange(self.folds_count):
+        # TODO: Activate for loop later!
+        i = 2
+        # A task that will merge all folds except the one left out for testing,
+        # ... into a training data set, and just pass on the one left out, as
+        # the test data set.
+        create_folds = sl.new_task(CreateFolds, 'create_fold_%d' % i, self,
+                fold_index = 2,
+                folds_count = self.folds_count,
+                seed = 0.637)
 
-            # Plugging in the 'generic' train components, for SVM/LibLinear, here
-            train = sl.new_task(MockTrain, 'train_svm', self)
-            train.in_traindata = merge
+        create_folds.in_dataset = rawdata.out_dataset
 
-            # Plugging in the 'generic' predict components, for SVM/LibLinear, here
-            predict = sl.new_task(MockPredict, 'predict', self)
-            predict.in_svmmodel = train
+        # Plugging in the 'generic' train components, for SVM/LibLinear, here
+        train = sl.new_task(MockTrain, 'train_svm', self)
+        train.in_traindata = create_folds.out_traindata
 
-            fold_tasks[i] = {}
-            fold_tasks[i]['merge'] = merge
-            fold_tasks[i]['train'] = train
-            fold_tasks[i]['predict'] = predict
+        # Plugging in the 'generic' predict components, for SVM/LibLinear, here
+        predict = sl.new_task(MockPredict, 'predict', self)
+        predict.in_testdata = create_folds.out_testdata
+        predict.in_svmmodel = train
+
+        fold_tasks[i] = {}
+        fold_tasks[i]['create_folds'] = create_folds
+        fold_tasks[i]['train'] = train
+        fold_tasks[i]['predict'] = predict
 
         # Collect the prediction targets from the branches above, into one dict, to feed
         # into the specialized assess component below
-        predict_targets = { i : fold_tasks[i]['predict'] for i in xrange(self.folds_count) }
-
-        assess = sl.new_task(MockAssessCrossVal, 'assess', self, folds_count=self.folds_count)
-        assess.in_predict_targets = predict_targets
+        #predict_targets = { i : fold_tasks[i]['predict'] for i in xrange(self.folds_count) }
+        #assess = sl.new_task(MockAssessCrossVal, 'assess', self, folds_count=self.folds_count)
+        #assess.in_predict_targets = predict_targets
 
         return locals()[self.task]
 
