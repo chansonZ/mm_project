287e1e16b51d9306b9eb9b28254181cc4804029a
Samuel Lampa <samuel.lampa@gmail.com>
2015-09-02 22:53:17 +0200

    Better formatting of run script

--- exp/20150627-crossval/runmmlin.sh
+++ exp/20150627-crossval/runmmlin.sh
@@ -1,1 +1,8 @@
-python wfmmlin.py MMLinear --replicate-id=r1 --test-size=10 --train-size=50 --sampling-method=random --lin-type=0 --lin-cost=1000 --slurm-project=b2013262
+python wfmmlin.py MMLinear \
+    --replicate-id=r1 \
+    --test-size=10 \
+    --train-size=50 \
+    --sampling-method=random \
+    --lin-type=0 \
+    --lin-cost=1000 \
+    --slurm-project=b2013262
