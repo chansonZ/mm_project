f227096f08f279c04ae169e46a5b07b2245358e6
Samuel Lampa <samuel.lampa@gmail.com>
2015-09-09 18:28:32 +0200

    Fixing bugs with runmode. Adding new create folds related tasks

--- exp/20150627-crossval/wffindcost.py
+++ exp/20150627-crossval/wffindcost.py
@@ -1,4 +1,5 @@
 from mmcomp import *
+import logging
 import luigi
 import sciluigi as sl
 import time
@@ -6,6 +7,8 @@ import time
 # ====================================================================================================
 #  New components for Cross-validation - May 8, 2015
 # ====================================================================================================
+
+log = logging.getLogger('sciluigi-interface')
 
 class CrossValidate(sl.WorkflowTask):
     '''
@@ -18,8 +21,9 @@ class CrossValidate(sl.WorkflowTask):
     replicate_id = luigi.Parameter()
     min_height = luigi.Parameter()
     max_height = luigi.Parameter()
-    test_size = luigi.Parameter(default='50000')
-    train_size = luigi.Parameter(default='rest')
+    test_size = luigi.Parameter()
+    train_size = luigi.Parameter()
+    randomdatasize_mb = luigi.IntParameter()
     slurm_project = luigi.Parameter(default='b2013262')
     runmode = luigi.Parameter()
 
@@ -33,15 +37,17 @@ class CrossValidate(sl.WorkflowTask):
         else:
             raise Exception('Runmode is none of local, hpc, nor mpi. Please fix and try again!')
 
+        # ----------------------------------------------------------------
         mmtestdata = self.new_task('mmtestdata', ExistingSmiles,
                 replicate_id=self.replicate_id,
                 dataset_name=self.dataset_name)
+        # ----------------------------------------------------------------
         gensign = self.new_task('gensign', GenerateSignaturesFilterSubstances,
                 replicate_id=self.replicate_id,
                 min_height = self.min_height,
                 max_height = self.max_height,
                 slurminfo = sl.SlurmInfo(
-                    runmode=self.runmode,
+                    runmode=runmode,
                     project=self.slurm_project,
                     partition='core',
                     cores='8',
@@ -49,8 +55,12 @@ class CrossValidate(sl.WorkflowTask):
                     jobname='mmgensign',
                     threads='8'
                 ))
+        gensign.in_smiles = mmtestdata.out_smiles
+        # ----------------------------------------------------------------
         replcopy = self.new_task('replcopy', CreateReplicateCopy,
                 replicate_id=self.replicate_id)
+        replcopy.in_file = gensign.out_signatures
+        # ----------------------------------------------------------------
         samplett = self.new_task('sampletraintest', SampleTrainAndTest,
                 replicate_id=self.replicate_id,
                 sampling_method='random',
@@ -58,7 +68,7 @@ class CrossValidate(sl.WorkflowTask):
                 test_size=self.test_size,
                 train_size=self.train_size,
                 slurminfo = sl.SlurmInfo(
-                    runmode=self.runmode,
+                    runmode=runmode,
                     project='b2013262',
                     partition='core',
                     cores='12',
@@ -66,10 +76,12 @@ class CrossValidate(sl.WorkflowTask):
                     jobname='mmsampletraintest',
                     threads='1'
                 ))
+        samplett.in_signatures = replcopy.out_copy
+        # ----------------------------------------------------------------
         sprstrain = self.new_task('sparsetrain', CreateSparseTrainDataset,
                 replicate_id=self.replicate_id,
                 slurminfo = sl.SlurmInfo(
-                    runmode=self.runmode,
+                    runmode=runmode,
                     project=self.slurm_project,
                     partition='node',
                     cores='16',
@@ -77,9 +89,11 @@ class CrossValidate(sl.WorkflowTask):
                     jobname='mmsparsetrain',
                     threads='16'
                 ))
+        sprstrain.in_traindata = samplett.out_traindata
+        # ----------------------------------------------------------------
         gunzip = self.new_task('gunzip_sparsetrain', UnGzipFile,
                 slurminfo = sl.SlurmInfo(
-                    runmode=self.runmode,
+                    runmode=runmode,
                     project=self.slurm_project,
                     partition='core',
                     cores='1',
@@ -87,24 +101,47 @@ class CrossValidate(sl.WorkflowTask):
                     jobname='gunzip_sparsetrain',
                     threads='1'
                 ))
+        gunzip.in_gzipped = sprstrain.out_sparse_traindata
+        # ----------------------------------------------------------------
         cntlines = self.new_task('countlines', CountLines,
                 slurminfo = sl.SlurmInfo(
-                    runmode=self.runmode,
+                    runmode=runmode,
                     project=self.slurm_project,
-                    partition='devcore',
+                    partition='core',
                     cores='1',
                     time='15:00',
                     jobname='gunzip_sparsetrain',
                     threads='1'
                 ))
-
-        # Connect tasks by their inports and outports
-        gensign.in_smiles = mmtestdata.out_smiles
-        replcopy.in_file = gensign.out_signatures
-        samplett.in_signatures = replcopy.out_copy
-        sprstrain.in_traindata = samplett.out_traindata
-        gunzip.in_gzipped = sprstrain.out_sparse_traindata
         cntlines.in_file = sprstrain.out_sparse_traindata
+        # ----------------------------------------------------------------
+        genrandomdata= self.new_task('genrandomdata', CreateRandomData,
+                size_mb=self.randomdatasize_mb,
+                replicate_id=self.replicate_id,
+                slurminfo = sl.SlurmInfo(
+                    runmode=runmode,
+                    project=self.slurm_project,
+                    partition='core',
+                    cores='1',
+                    time='1:00:00',
+                    jobname='genrandomdata',
+                    threads='1'
+                ))
+        genrandomdata.in_basepath = gunzip.out_ungzipped
+        # ----------------------------------------------------------------
+        shufflelines = self.new_task('shufflelines', ShuffleLines,
+                slurminfo = sl.SlurmInfo(
+                    runmode=runmode,
+                    project=self.slurm_project,
+                    partition='core',
+                    cores='1',
+                    time='15:00',
+                    jobname='shufflelines',
+                    threads='1'
+                ))
+        shufflelines.in_randomdata = genrandomdata.out_random
+        shufflelines.in_file = gunzip.out_ungzipped
+        # ----------------------------------------------------------------
 
         tasks = {}
         costseq = [str(int(10**p)) for p in xrange(1,9)]
@@ -118,20 +155,23 @@ class CrossValidate(sl.WorkflowTask):
                         folds_count = self.folds_count,
                         seed = 0.637,
                         slurminfo = sl.SlurmInfo(
-                            runmode=self.runmode,
+                            runmode=runmode,
                             project=self.slurm_project,
                             partition='core',
                             cores='1',
-                            time='1:00:00',
+                            time='5:00',
                             jobname='create_fold%02d_cost%s' % (fold_idx, cost),
                             threads='1'
                         ))
+                create_folds.in_dataset = shufflelines.out_shuffled
+                create_folds.in_linecount = cntlines.out_linecount
+                # -------------------------------------------------
                 train_lin = self.new_task('trainlin_fold_%d_cost_%s' % (fold_idx, cost), TrainLinearModel,
                         replicate_id = self.replicate_id,
                         lin_type = '0', # 0 = Regression
                         lin_cost = cost,
                         slurminfo = sl.SlurmInfo(
-                            runmode=self.runmode,
+                            runmode=runmode,
                             project=self.slurm_project,
                             partition='core',
                             cores='1',
@@ -139,10 +179,12 @@ class CrossValidate(sl.WorkflowTask):
                             jobname='trnlin_f%02d_c%010d' % (fold_idx, int(cost)),
                             threads='1'
                         ))
+                train_lin.in_traindata = create_folds.out_traindata
+                # -------------------------------------------------
                 pred_lin = self.new_task('predlin_fold_%d_cost_%s' % (fold_idx, cost), PredictLinearModel,
                         replicate_id = self.replicate_id,
                         slurminfo = sl.SlurmInfo(
-                            runmode=self.runmode,
+                            runmode=runmode,
                             project=self.slurm_project,
                             partition='core',
                             cores='1',
@@ -150,10 +192,13 @@ class CrossValidate(sl.WorkflowTask):
                             jobname='predlin_f%02d_c%010d' % (fold_idx, int(cost)),
                             threads='1'
                         ))
+                pred_lin.in_linmodel = train_lin.out_linmodel
+                pred_lin.in_sparse_testdata = create_folds.out_testdata
+                # -------------------------------------------------
                 assess_lin = self.new_task('assesslin_fold_%d_cost_%s' % (fold_idx, cost), AssessLinearRMSD,
                         lin_cost = cost,
                         slurminfo = sl.SlurmInfo(
-                            runmode=self.runmode,
+                            runmode=runmode,
                             project=self.slurm_project,
                             partition='core',
                             cores='1',
@@ -161,17 +206,10 @@ class CrossValidate(sl.WorkflowTask):
                             jobname='assesslin_f%02d_c%010d' % (fold_idx, int(cost)),
                             threads='1'
                         ))
-
-                # Connect tasks
-                create_folds.in_dataset = gunzip.out_ungzipped
-                create_folds.in_linecount = cntlines.out_linecount
-                train_lin.in_traindata = create_folds.out_traindata
-                pred_lin.in_linmodel = train_lin.out_linmodel
-                pred_lin.in_sparse_testdata = create_folds.out_testdata
                 assess_lin.in_linmodel = train_lin.out_linmodel
                 assess_lin.in_sparse_testdata = create_folds.out_testdata
                 assess_lin.in_prediction = pred_lin.out_prediction
-
+                # -------------------------------------------------
                 tasks[cost][fold_idx] = {}
                 tasks[cost][fold_idx]['create_folds'] = create_folds
                 tasks[cost][fold_idx]['train_linear'] = train_lin
