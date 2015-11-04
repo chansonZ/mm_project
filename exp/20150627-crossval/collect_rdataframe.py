a907d2e051d2fce018364345ec757349cd8860a7
Samuel Lampa <samuel.lampa@gmail.com>
2015-11-04 14:52:44 +0100

    Started working on script to collect an R data frame for plotting

---
+++ exp/20150627-crossval/collect_rdataframe.py
@@ -0,0 +1,32 @@
+import glob
+import sciluigi as sl
+import csv
+import re
+
+dataframe = []
+def main():
+    glob_pattern = 'data/*ungz.s12*.rmsd'
+    rmsd_paths = glob.glob(glob_pattern)
+    for rp in rmsd_paths:
+        datarow = {}
+        ms = re.findall('(solubility|acd_logd).smi.h1_3.sign.(r[0-9]).([0-9]+)_([0-9]+|rest)_rand_trn.csr.ungz.s12_c([0-9\.]+).(lin|svm)mdl.pred.rmsd', rp)
+        m = ms[0]
+        datarow['dataset'] = m[0]
+        datarow['replicate'] = m[1]
+        datarow['test_size'] = m[2]
+        datarow['training_size'] = m[3]
+        datarow['cost'] = m[4]
+        datarow['learning_method'] = m[5]
+        with open(rp) as rf:
+            rd = sl.util.recordfile_to_dict(rf)
+            datarow['rmsd'] = rd['rmsd']
+            datarow['cost'] = rd['cost']
+            dataframe.append(datarow)
+    with open('rdataframe.csv', 'w') as fh:
+        csvwrt = csv.writer(fh)
+        csvwrt.writerow(['dataset', 'learning_method', 'training_size', 'replicate', 'rmsd', 'model_creation_time', 'cost'])
+        for row in dataframe:
+            csvwrt.writerow([row['dataset'], row['learning_method'], row['training_size'], row['replicate'], row['rmsd'], 'N/A', row['cost']])
+
+if __name__ == '__main__':
+    main()
